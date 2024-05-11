fcef5a77c9988af46a4e9f5b2cab5425976ce579
tww001381 <147319434+tww001381@users.noreply.github.com>
2024-05-11 17:22:02 +0900

    Add files via upload

---
+++ Digda.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CatchDigda
+{
+    public partial class Digda : Form
+    {
+        private bool started = false;
+
+        private int remainingSeconds = 60;
+        private int score = 0;
+
+        private List<PictureBox> holes = new List<PictureBox>();
+        private Image moleImage = Properties.Resources.Digda;
+        private Image advancedMoleImage = Properties.Resources.Dugtrio;
+        private Image emptyGroundImage = Properties.Resources.emptyground;
+        private Random random = new Random();
+
+        public Digda()
+        {
+            InitializeComponent();
+            InitializeHoles();
+        }
+
+        private void InitializeHoles()
+        {
+            holes.Add(pictureBox1);
+            holes.Add(pictureBox2);
+            holes.Add(pictureBox3);
+            holes.Add(pictureBox4);
+            holes.Add(pictureBox5);
+            holes.Add(pictureBox6);
+            holes.Add(pictureBox7);
+            holes.Add(pictureBox8);
+            holes.Add(pictureBox9);
+
+            pgbTime.Minimum = 0;
+            pgbTime.Maximum = remainingSeconds;
+            pgbTime.Value = (60 - remainingSeconds);
+
+            foreach (PictureBox p in holes) {
+                p.Image = emptyGroundImage;
+                p.Click += HoleClicked;
+            }
+        }
+
+        private void tmrRound_Tick(object sender, EventArgs e)
+        {
+            remainingSeconds--;
+            lblRoundTimer.Text = string.Format("{0:D2} : {1:D2}", remainingSeconds / 60, remainingSeconds % 60);
+            pgbTime.Value = 60 - remainingSeconds;
+        }
+
+        private void HoleClicked(object sender, EventArgs e)
+        {
+            PictureBox clickedHole = (PictureBox)sender;
+
+            if (clickedHole.Image == emptyGroundImage) { 
+                return;
+            }
+
+            int clickedscore = (int)clickedHole.Tag;
+            this.score += clickedscore;
+
+            lblScore.Text = "Score : " + this.score.ToString() + " (+ " + clickedscore.ToString() + "!)";
+
+            // 점수 반영 이후, 사라지는 과정 (순서 변경 x)
+            clickedHole.Image = emptyGroundImage;
+            clickedHole.Tag = null;
+        }
+
+        private void ShowMole(PictureBox hole, Image moleImage, int score) {
+            hole.Image = moleImage;
+            hole.Tag = score;
+        }
+
+        private async void StartGame()
+        {
+            started = true;
+            score = 0;
+            foreach (PictureBox mole in holes) {
+                mole.Visible = true;
+                mole.Enabled = true;
+            }
+
+            while (true)
+            {
+                int remainingTime = remainingSeconds;
+                int waitTime = 0;
+
+                if (remainingTime > 30) {
+                    waitTime = 1000;
+                } 
+                else if (remainingTime > 15) {
+                    waitTime = 700;
+                }
+                else {
+                    waitTime = 500;
+                }
+
+                List<PictureBox> selectedHoles = new List<PictureBox>();
+
+                while (selectedHoles.Count < 2) {
+                    PictureBox moleHole = holes[random.Next(holes.Count)];
+                    if (!selectedHoles.Contains(moleHole))
+                    {
+                        selectedHoles.Add(moleHole);
+                    }
+                }
+
+                foreach (PictureBox hole in selectedHoles)
+                {
+                    int moletype = random.Next(100);
+                    if (moletype < 20) { // 20% 확률
+                        ShowMole(hole, advancedMoleImage, 3);
+                    }
+                    else { // 80% 확률
+                        ShowMole(hole, moleImage, 1);
+                    }
+                }
+                await Task.Delay(waitTime);
+
+                foreach (PictureBox hole in holes) {
+                    hole.Image = emptyGroundImage;
+                }
+
+                if (remainingSeconds == 0)
+                {
+                    ResetGame();
+                    MessageBox.Show("Time Finished!\n\nScore : " + (score).ToString(), "Finished!", MessageBoxButtons.OK);
+                }
+            }
+        }
+
+        private void ResetGame()
+        {
+            started = false;
+
+            tmrRound.Stop();
+            remainingSeconds = 60;
+            foreach (PictureBox mole in holes) {
+                mole.Visible = false;
+                mole.Enabled = false;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e) {
+            tmrRound.Start();
+            StartGame();
+        }
+    }
+}
