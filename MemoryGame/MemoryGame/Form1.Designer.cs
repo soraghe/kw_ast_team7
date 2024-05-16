651d9bd6187dba7fcbd5442fe90ef6b5d9b0ddfb
Rahyuni <144291487+Rahyuni@users.noreply.github.com>
2024-05-16 20:24:25 +0900

    Update files in MemoryGame

---
+++ MemoryGame/MemoryGame/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace MemoryGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblTimeLeft = new System.Windows.Forms.Label();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(423, 187);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(165, 18);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "Match or MisMatch";
+            // 
+            // lblTimeLeft
+            // 
+            this.lblTimeLeft.AutoSize = true;
+            this.lblTimeLeft.Font = new System.Drawing.Font("나눔스퀘어 네오 Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblTimeLeft.Location = new System.Drawing.Point(412, 276);
+            this.lblTimeLeft.Name = "lblTimeLeft";
+            this.lblTimeLeft.Size = new System.Drawing.Size(176, 26);
+            this.lblTimeLeft.TabIndex = 1;
+            this.lblTimeLeft.Text = "Time Left: 30";
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnRestart.Font = new System.Drawing.Font("나눔스퀘어 네오 Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnRestart.Location = new System.Drawing.Point(417, 59);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(180, 59);
+            this.btnRestart.TabIndex = 2;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = false;
+            this.btnRestart.Click += new System.EventHandler(this.RestartGameEvent);
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Interval = 1000;
+            this.GameTimer.Tick += new System.EventHandler(this.TimerEvent);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(395, 317);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(213, 209);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 553);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.lblTimeLeft);
+            this.Controls.Add(this.lblStatus);
+            this.Name = "Form1";
+            this.Text = "Memory Game Form";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblTimeLeft;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ MemoryGame/MemoryGame/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MemoryGame
+{
+    public partial class Form1 : Form
+    {
+
+        List<int> numbers = new List<int> { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10 };
+        string firstChoice;
+        string secondChoice;
+        int tries;
+        List<PictureBox> pictures = new List<PictureBox>(); // 픽쳐박스 객체의 리스트
+        PictureBox picA;
+        PictureBox picB;
+        int totalTime = 60;
+        int countDownTime;
+        bool gameOver = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            MessageBox.Show("졸업 시험 시작합니다.");
+            LoadPictures();
+        }
+
+        private void TimerEvent(object sender, EventArgs e)
+        {
+            countDownTime--;
+
+            lblTimeLeft.Text = "Time Left : " + countDownTime;
+
+            if (countDownTime < 1)
+            {
+                GameOver("이대로 졸업은 불가합니다. 재도전하세요.");
+
+                foreach (PictureBox x in pictures)
+                {
+                    if (x.Tag != null)
+                    {
+                            string fileName = (string)x.Tag + ".png";
+                            string filePath = @"C:\C#project(3-1)\MemoryGame\MemoryGame\bin\Debug\" + fileName;
+                            x.Image = Image.FromFile(filePath);   
+                    }
+                }
+            }
+        }
+
+        private void RestartGameEvent(object sender, EventArgs e)
+        {
+            RestartGame();
+        }
+
+        private void LoadPictures()
+        {
+            int leftPos = 30;
+            int topPos = 30;
+            int rows = 0;
+
+            for (int i = 0; i < 20; i++)
+            {
+                PictureBox newPic = new PictureBox();
+                newPic.Height = 50;
+                newPic.Width = 50;
+                newPic.BackColor = Color.LightGray;
+                newPic.SizeMode = PictureBoxSizeMode.StretchImage;
+                newPic.Click += NewPic_Click;
+                pictures.Add(newPic);
+
+                if (rows < 4)
+                {
+                    rows++;
+                    newPic.Left = leftPos;
+                    newPic.Top = topPos;
+                    this.Controls.Add(newPic);
+                    leftPos = leftPos + 60;
+                }
+
+                if (rows == 4)
+                {
+                    leftPos = 30;
+                    topPos += 60;
+                    rows = 0;
+                }
+            }
+
+            RestartGame();
+        }
+
+        private void NewPic_Click(object sender, EventArgs e)
+        {
+            if (gameOver)
+            {
+                return; // dont register a click if the game is over
+            }
+
+            if (firstChoice == null)
+            {
+                picA = sender as PictureBox;
+                if (picA.Tag != null && picA.Image == null)
+                {
+                    string fileName = (string)picA.Tag + ".png";
+                    string filePath = @"C:\C#project(3-1)\MemoryGame\MemoryGame\bin\Debug\" + fileName;
+                    picA.Image = Image.FromFile(filePath);
+
+                    firstChoice = (string)picA.Tag;
+                }
+            }
+            else if (secondChoice == null)
+            {
+                picB = sender as PictureBox;
+
+                if (picB.Tag != null && picB.Image == null)
+                {
+                    string fileName = (string)picB.Tag + ".png";
+                    string filePath = @"C:\C#project(3-1)\MemoryGame\MemoryGame\bin\Debug\" + fileName;
+                    picB.Image = Image.FromFile(filePath);
+
+                    secondChoice = (string)picB.Tag;
+                }
+            }
+            else
+            {
+                CheckPictures(picA, picB);
+            }
+        }
+
+        private void RestartGame()
+        {
+            var randomList = numbers.OrderBy(x => Guid.NewGuid()).ToList(); // randomise the original list
+            numbers = randomList;
+
+            for (int i = 0; i < pictures.Count; i++)
+            {
+                pictures[i].Image = null;
+                pictures[i].Tag = numbers[i].ToString();
+            }
+
+            tries = 0;
+            lblStatus.Text = "Mismatched: " + tries + " times.";
+            lblTimeLeft.Text = "Time Left: " + totalTime;
+            gameOver = false;
+            GameTimer.Start();
+            countDownTime = totalTime;
+        }
+
+        private void CheckPictures(PictureBox A, PictureBox B)
+        {
+            if (firstChoice == secondChoice)
+            {
+                A.Tag = null;
+                B.Tag = null;
+            }
+            else
+            {
+                tries++;
+                lblStatus.Text = "Mismatched " + tries + " times.";
+            }
+
+            firstChoice = null;
+            secondChoice = null;
+
+            foreach (PictureBox pics in pictures.ToList())
+            {
+                if (pics.Tag != null)
+                {
+                    pics.Image = null;
+                }
+            }
+
+            // lets check if all of the items have been solved
+            if (pictures.All(o => o.Tag == pictures[0].Tag))
+            {
+                GameOver("졸업 요건 달성 !");
+            }
+        }
+
+        private void GameOver(string msg)
+        {
+            GameTimer.Stop();
+            gameOver = true;
+            MessageBox.Show(msg);
+        }
+    }
+}
