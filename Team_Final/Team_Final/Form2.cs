4e0282a35ad0cf9d359dfbeba4a5226c002befdf
Rahyuni <144291487+Rahyuni@users.noreply.github.com>
2024-06-16 02:28:56 +0900

    remove .git files

---
+++ Team_Final/Team_Final/Form2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Team_Final
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void picBox_RH_Click(object sender, EventArgs e)
+        {
+            MemoryGame.Form1 form1 = new MemoryGame.Form1();
+            form1.Show();
+        }
+
+        private void picBox_TW_Click(object sender, EventArgs e)
+        {
+            CatchDigda.Digda digda = new CatchDigda.Digda();
+            digda.Show();
+        }
+    }
+}
