4ba163adec5afb604a1c0966bb3f782e99f73b16
tww <taeu1381@naver.com>
2024-06-17 02:36:33 +0900

    Finale version

---
+++ CatchDigda/Digda.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CatchDigda
+{
+    public partial class Digda : Form
+    {
+        private bool started = false;
+
+        private int remainingSeconds = 60;
+        private int score = 0;
+        public int final_score = 0;
+        private int CreatenormalMoleCount = 0;
+        private int CreateadvancedMoleCount = 0;
+        private int ClickednoramlMoleCount = 0;
+        private int ClickedadvancedMoleCount = 0;
+
+        private List<PictureBox> holes = new List<PictureBox>();
+        private Image moleImage = Properties.Resources.Digda;
+        private Image advancedMoleImage = Properties.Resources.Dugtrio;
+        private Image emptyGroundImage = Properties.Resources.emptyground;
+        private Random random = new Random();
+
+        public Digda()
+        {
+            InitializeComponent();
+            InitializeHoles();
+        }
+
+        private void InitializeHoles()
+        {
+            holes.Add(pictureBox1);
+            holes.Add(pictureBox2);
+            holes.Add(pictureBox3);
+            holes.Add(pictureBox4);
+            holes.Add(pictureBox5);
+            holes.Add(pictureBox6);
+            holes.Add(pictureBox7);
+            holes.Add(pictureBox8);
+            holes.Add(pictureBox9);
+
+            pgbTime.Minimum = 0;
+            pgbTime.Maximum = remainingSeconds;
+            pgbTime.Value = (60 - remainingSeconds);
+
+            foreach (PictureBox p in holes) {
+                p.Image = emptyGroundImage;
+                p.Click += HoleClicked;
+            }
+        }
+
+        private void tmrRound_Tick(object sender, EventArgs e)
+        {
+            remainingSeconds--;
+            lblRoundTimer.Text = string.Format("{0:D2} : {1:D2}", remainingSeconds / 60, remainingSeconds % 60);
+            pgbTime.Value = 60 - remainingSeconds;
+        }
+
+        private void HoleClicked(object sender, EventArgs e)
+        {
+            PictureBox clickedHole = (PictureBox)sender;
+
+            if (clickedHole.Image == emptyGroundImage) { 
+                return;
+            }
+
+            if (clickedHole.Image == moleImage) {
+                ClickednoramlMoleCount++;
+            } else if (clickedHole.Image == advancedMoleImage) {
+                ClickedadvancedMoleCount++;
+            }
+
+            int clickedscore = (int)clickedHole.Tag;
+            this.score += clickedscore;
+
+            lblScore.Text = "Score : " + this.score.ToString() + " ( +" + clickedscore.ToString() + " ! )";
+            lblNormalCount.Text = ": " + ClickednoramlMoleCount.ToString() + " / " + CreatenormalMoleCount.ToString();
+            lblAdvanceCount.Text = ": " + ClickedadvancedMoleCount.ToString() + " / " + CreateadvancedMoleCount.ToString();
+
+            // 점수 반영 이후, 사라지는 과정 (순서 변경 x)
+            clickedHole.Image = emptyGroundImage;
+            clickedHole.Tag = null;
+        }
+
+        private void ShowMole(PictureBox hole, Image molePic, int score) {
+            hole.Image = molePic;
+            hole.Tag = score;
+            
+            if (molePic == moleImage)
+            {
+                CreatenormalMoleCount++;
+            }
+            else if (molePic == advancedMoleImage)
+            {
+                CreateadvancedMoleCount++;
+            }
+        }
+        private async void StartGame()
+        {
+            started = true;
+            score = 0;
+            foreach (PictureBox mole in holes) {
+                mole.Visible = true;
+                mole.Enabled = true;
+            }
+            if (remainingSeconds != 0) { 
+                btnStart.Enabled = false;
+            }
+
+            while (true)
+            {
+                int remainingTime = remainingSeconds;
+                int waitTime = 0;
+
+                if (remainingTime > 30) {
+                    waitTime = 1000;
+                } 
+                else if (remainingTime > 15) {
+                    waitTime = 700;
+                }
+                else {
+                    waitTime = 500;
+                }
+
+                List<PictureBox> selectedHoles = new List<PictureBox>();
+
+                while (selectedHoles.Count < 2) {
+                    PictureBox moleHole = holes[random.Next(holes.Count)];
+                    if (!selectedHoles.Contains(moleHole))
+                    {
+                        selectedHoles.Add(moleHole);
+                    }
+                }
+
+                foreach (PictureBox hole in selectedHoles)
+                {
+                    int moletype = random.Next(100);
+                    if (moletype < 20) { // 20% 확률
+                        ShowMole(hole, advancedMoleImage, 3);
+                    }
+                    else { // 80% 확률
+                        ShowMole(hole, moleImage, 1);
+                    }
+                }
+                await Task.Delay(waitTime);
+
+                foreach (PictureBox hole in holes) {
+                    hole.Image = emptyGroundImage;
+                }
+
+                if (remainingSeconds == 0)
+                {
+                    ResetGame();
+
+                    double ratio = 100.0 / 186.0;
+                    final_score = (int)Math.Round(this.score * ratio);
+
+                    string message = string.Format("Time Finished!\n\nScore: {0}\nFinal Score: {1}", score, final_score);
+                    MessageBox.Show(message, "게임 종료!", MessageBoxButtons.OK);
+                    btnStart.Enabled = true;
+                }
+            }
+        }
+
+        private void ResetGame()
+        {
+            started = false;
+
+            tmrRound.Stop();
+            remainingSeconds = 60;
+            foreach (PictureBox mole in holes) {
+                mole.Visible = false;
+                mole.Enabled = false;
+            }
+            CreatenormalMoleCount = 0;
+            CreateadvancedMoleCount = 0;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e) {
+            tmrRound.Start();
+            StartGame();
+        }
+    }
+}
---
+++ MemoryGame/MemoryGame/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace MemoryGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblTimeLeft = new System.Windows.Forms.Label();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(458, 66);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(165, 18);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "Match or MisMatch";
+            // 
+            // lblTimeLeft
+            // 
+            this.lblTimeLeft.AutoSize = true;
+            this.lblTimeLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblTimeLeft.Location = new System.Drawing.Point(456, 112);
+            this.lblTimeLeft.Name = "lblTimeLeft";
+            this.lblTimeLeft.Size = new System.Drawing.Size(165, 29);
+            this.lblTimeLeft.TabIndex = 1;
+            this.lblTimeLeft.Text = "Time Left: 60";
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnRestart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnRestart.Location = new System.Drawing.Point(452, 164);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(180, 59);
+            this.btnRestart.TabIndex = 2;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = false;
+            this.btnRestart.Click += new System.EventHandler(this.RestartGameEvent);
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Interval = 1000;
+            this.GameTimer.Tick += new System.EventHandler(this.TimerEvent);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(721, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(270, 253);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(430, 295);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(626, 207);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "게임 규칙 (How to Play?)";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(6, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(607, 138);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1. 총 1분, 60초 간 진행됩니다.\r\n\r\n2. 타일을 모두 맞춘 후, 한번 더 클릭해야 게임 클리어 결과가 저장됩니다.\r\n\r\n3. 통과하지 못" +
+    "했다면 Restart 버튼을 눌러 재도전하세요 !\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1079, 570);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.lblTimeLeft);
+            this.Controls.Add(this.lblStatus);
+            this.Name = "Form1";
+            this.Text = "Memory Game Form";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblTimeLeft;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
